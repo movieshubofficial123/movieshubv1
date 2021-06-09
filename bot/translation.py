4e749cdd91ce82c64f61b7f929d6e85b38b4d6a5
movieshubofficial123 <85605801+movieshubofficial123@users.noreply.github.com>
2021-06-09 13:12:48 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,10 +7,10 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Join @movieshubofficial123,@movies_Hubh
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@AbinCr
 </i>"""    
     
     HELP_TEXT = """
@@ -43,12 +43,12 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@movies_Hubh
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/AbinCr">AbinCr</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
